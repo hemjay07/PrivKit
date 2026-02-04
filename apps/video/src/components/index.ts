7520777b257313a50c594f1c4017e693e4a2f888
mujeeb-07 <mujeeb@mono.co>
2026-02-04 19:00:55 +0100

    Remove internal files from repo
    - Remove app_spec.txt (internal spec)
    - Remove prompts/ folder (agent instructions)
    - Remove apps/video/ (video generation assets)
    - Remove UNDERSTANDING_PRIVKIT docs (keep local via gitignore)

--- apps/video/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { FocusHighlight } from "./FocusHighlight";
-export { AnimatedCursor } from "./AnimatedCursor";
-export { TerminalAnimation } from "./TerminalAnimation";
-export { LogoReveal } from "./LogoReveal";
-export { TextReveal } from "./TextReveal";
-export { FeatureCard } from "./FeatureCard";
